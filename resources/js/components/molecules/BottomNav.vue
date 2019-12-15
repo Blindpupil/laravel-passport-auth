252e685405b14122edbb92f61392283aea5f441a
cesarmartinez <cesarmartinezd@gmail.com>
2019-12-15 17:01:46 +0100

    dashboard layout and recipes view

---
+++ resources/js/components/molecules/BottomNav.vue
@@ -0,0 +1,43 @@
+<template>
+	<v-bottom-navigation :value="activeBtn" color="primary elevation-0" shift hide-on-scroll absolute>
+		<v-btn size="large">
+			<span>Recipes</span>
+			<v-icon>{{ mdiRoomService }}</v-icon>
+		</v-btn>
+
+		<v-btn size="large">
+			<span>Pantry</span>
+			<v-icon>{{ mdiFridge }}</v-icon>
+		</v-btn>
+
+		<v-btn size="large">
+			<span>Lists</span>
+			<v-icon>{{ mdiClipboardList }}</v-icon>
+		</v-btn>
+
+		<v-btn size="large">
+			<span>Calendar</span>
+			<v-icon>{{ mdiCalendarMonth }}</v-icon>
+		</v-btn>
+	</v-bottom-navigation>
+</template>
+
+<script>
+import { mdiCalendarMonth, mdiClipboardList, mdiFridge, mdiRoomService } from '@mdi/js'
+
+export default {
+  name: 'BottomNav',
+  data: () => ({
+    mdiFridge,
+    mdiClipboardList,
+    mdiCalendarMonth,
+    mdiRoomService,
+    drawer: null,
+    activeBtn: 0,
+  }),
+}
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/components/pages/Dashboard.vue
+++ resources/js/components/pages/Dashboard.vue
@@ -1,20 +1,37 @@
 <template>
-	<div>
-		Dashboard goes here
+	<dashboard-layout>
+		<template v-slot:header>
+			Recipes
+		</template>
+		<v-row>
+			<v-col cols="12">
+				<v-row align-content="center" class="recipe-view-content-height">
+					<v-col cols="12" class="text-center">
+						<v-icon size="90">{{ mdiPackageVariant }}</v-icon>
+					</v-col>
+					<v-col cols="12" class="text-center font-weight-medium">
+						No recipes saved at the moment
+					</v-col>
+				</v-row>
+			</v-col>
 
-		<v-btn @click="logout">Logout</v-btn>
-	</div>
+			<v-col cols="12">
+				<v-btn block large color="primary" class="my-6 font-weight-bold">
+					Add a recipe
+				</v-btn>
+			</v-col>
+		</v-row>
+	</dashboard-layout>
 </template>
 
 <script>
+import { mdiPackageVariant } from '@mdi/js'
+
 export default {
   name: 'Dashboard',
-  methods: {
-    async logout() {
-      await this.$store.dispatch('logout')
-      this.$router.push('/')
-    },
-  },
+  data: () => ({
+    mdiPackageVariant,
+  }),
 }
 </script>
 
